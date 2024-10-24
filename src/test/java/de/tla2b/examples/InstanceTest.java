0235f2165b82e84ebad0bba61e10a1d31d2c86ad
Jan Gruteser <jan.gruteser@hhu.de>
2024-10-24 09:47:29 +0200

    enable test

--- src/test/java/de/tla2b/examples/InstanceTest.java
+++ src/test/java/de/tla2b/examples/InstanceTest.java
@@ -1,12 +1,10 @@
 package de.tla2b.examples;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static de.tla2b.util.TestUtil.runModule;
 
 public class InstanceTest {
-
 
 	@Test
 	public void testInstanceNoName() throws Exception {
@@ -14,7 +12,6 @@ public class InstanceTest {
 		runModule(file);
 	}
 
-	@Ignore
 	@Test
 	public void testInstance() throws Exception {
 		String file = "src/test/resources/examples/instance/Counter/OneInstanced.tla";
