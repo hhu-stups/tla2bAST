feba1821fa6cbc7c76fc1a8e742a01eb929be617
Jan Gruteser <jan.gruteser@hhu.de>
2024-12-22 11:19:04 +0100

    add tests for instance transformation

--- src/test/java/de/tla2b/examples/InstanceTest.java
+++ src/test/java/de/tla2b/examples/InstanceTest.java
@@ -1,5 +1,6 @@
 package de.tla2b.examples;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static de.tla2b.util.TestUtil.runModule;
@@ -13,8 +14,46 @@ public class InstanceTest {
 	}
 
 	@Test
-	public void testInstance() throws Exception {
+	public void testConstantSubstitution1() throws Exception {
+		String file = "src/test/resources/examples/instance/constantSubstitution/InstanceOpArg3.tla";
+		runModule(file);
+	}
+
+	@Test
+	public void testConstantSubstitution2() throws Exception {
+		String file = "src/test/resources/examples/instance/constantSubstitution/InstanceValue2.tla";
+		runModule(file);
+	}
+
+	@Test
+	public void testOneInstanced() throws Exception {
 		String file = "src/test/resources/examples/instance/Counter/OneInstanced.tla";
+		runModule(file);
+	}
+
+	@Test
+	public void testTwoInstanced() throws Exception {
+		String file = "src/test/resources/examples/instance/Counter/TwoInstanced.tla";
+		runModule(file);
+	}
+
+	@Test
+	public void testLetInstanced() throws Exception {
+		String file = "src/test/resources/examples/instance/let/InstanceLet.tla";
+		runModule(file);
+	}
+
+	@Test
+	public void testOpArgInstanced() throws Exception {
+		String file = "src/test/resources/examples/instance/OpArg/OpArgInstanced.tla";
+		runModule(file);
+	}
+
+	@Ignore
+	@Test
+	public void testSimpleTwoInstanced() throws Exception {
+		// TODO: implement in Typechecker
+		String file = "src/test/resources/examples/instance/twoInstanced/first.tla";
 		runModule(file);
 	}
 }
